b30c636b364d9cd2e92fa4633e6f3e5a8e48bdce
Kminek42 <k.kminkowski@icloud.com>
2023-10-25 13:14:51 +0200

    saving stats in file

--- pomodoro.py
+++ pomodoro.py
@@ -1,5 +1,6 @@
 import time
 import simpleaudio as sa
+import datetime
 
 file = open("config.txt", "r")
 data = file.read()
@@ -14,26 +15,34 @@ wave_obj = sa.WaveObject.from_wave_file("./alarm.wav")
 
 t0 = time.time()
 
+def save_stat(filename, description):
+    formatted_datetime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    file = open("stats.txt", "a")
+    file.write(f"{formatted_datetime} {description}\n")
+    file.close()
+
+
+def block(block_name, duration):
+    t0 = time.time()
+
+    wave_obj.play()
+    print(f"{block_name} [{duration}min]")
+
+    while time.time() - t0 < duration * 60:
+        time.sleep(1)
+        if int(time.time()) % 10 == 0:
+            save_stat("stats.txt", block_name)
+
+break_n = 0
+
 while 2137:
-    for i in range(long_break_interval):
-        # lesson
-        play_obj = wave_obj.play()
-        print(f"Lesson [{lesson_time}min]")
-        t1 = time.time()
-        while time.time() - t1 < lesson_time * 60:
-            time.sleep(1)
+    # lesson
+    block("lesson", lesson_time)
 
-        # break
-        play_obj = wave_obj.play()
+    # break
+    if (break_n + 1) % long_break_interval:
+        block("break", short_break_time)
 
-        if i != long_break_interval - 1:
-            print(f"Break [{short_break_time}min]")
-            t1 = time.time()
-            while time.time() - t1 < short_break_time * 60:
-                time.sleep(1)
-
-        else:
-            print(f"Break [{long_break_time}min]")
-            t1 = time.time()
-            while time.time() - t1 < long_break_time * 60:
-                time.sleep(1)
+    else:
+        block("break", long_break_time)
